fb5219622fc29a911d50f305029f895fa6f64129
MisaelPecorari <42780892+MisaelPecorari@users.noreply.github.com>
2019-04-12 17:58:01 -0300

    Add files via upload

---
+++ API Rest/WorkDesdeCero/quickstartcomienzo/src/main/java/quickstart/quickstartcomienzo/App.java
@@ -0,0 +1,13 @@
+package quickstart.quickstartcomienzo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
